288a47f11f74fbb0ccd111bfeb027ac9df4ea87b
anasir514 <anasir514@gmail.com>
2021-01-19 13:18:59 +0000

    main

--- one_app/first_app/urls.py
+++ one_app/first_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "first_app"
+
 urlpatterns = [ 
+    path("", views.homepage, name="homepage"),
     path("register/", views.register, name="register"),
+    path("logout/", views.logout_request, name="logout"),
+    path("login/", views.login_request, name="login"),
+
     path('delete/<task_id>', views.delete_task, name='delete_task'),
     path('complete/<task_id>', views.complete_task, name='complete_task'),
     path('pending/<task_id>', views.pending_task, name='pending_task'),
